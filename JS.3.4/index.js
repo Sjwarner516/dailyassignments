6f4d1d0ea4162e2096faead3f71a00f9819e2a3d
SydniWarner <plions11@gmail.com>
2024-08-25 12:02:50 -0600

    completed js.3.4

--- JS.3.4/index.js
+++ JS.3.4/index.js
@@ -3,7 +3,10 @@
 // This function will take one parameter which will be a radius.
 // Return the area of the circle based on the radius.
 // area is 3.14 * r^2
-
+function circleArea (radius){
+    answer=3.14 * radius * radius;
+    return answer;
+}
 
 
 
@@ -13,7 +16,10 @@
 // This function will take one parameter which will be a radius.
 // Return the circumference of the circle based on the radius.
 // circumference is 2 * 3.14 * radius
-
+function circleCircumference (radius){
+    answer= 2 * 3.14 * radius;
+    return answer;
+}
 
 
 
@@ -23,24 +29,30 @@
 // Create a function called average.
 // This function will take three parameters which will be numbers.
 // Find the average of these numbers and return the value.
-
-
-
-
+function average (number1, number2, number3){
+    answer = (number1+number2+number3) / 3;
+    return answer;
+}
 
 // Problem 4
 // Create a function called combineTwoStrings.
 // This function will take two parameters.
 // return both of the strings concatenated(combined)
-
-
+function combineTwoStrings (one,two){
+    answer = one + two;
+    return answer;
+}
+    
 
 
 // Problem 5
 // Create a function called cubed.
 // This function will take one parameter.
 // Return the cubed number that is given from the parameter.
-
+function cubed (anynumber){
+    answer= anynumber * anynumber * anynumber;
+    return answer;
+}
 
 
 
@@ -48,7 +60,10 @@
 // Create a function called squared.
 // This function will take one parameter.
 // Return the squared number that is given from the parameter.
-
+function squared (anynumber){
+    answer=anynumber * anynumber;
+    return answer;
+}
 
 
 
@@ -57,5 +72,9 @@
 // This function will take one parameter which will be the radius.
 // Find the surface area given the radius 4 * 3.14 * r^2
 // Use your squared function to find the squared radius.
+function sphereArea (radius){
+    answer=4*3.14* squared(radius);
+    return answer;
 
+}
 
