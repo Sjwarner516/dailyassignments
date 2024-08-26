a4359294614354fa7dee4f8d419e98a12308a288
SydniWarner <plions11@gmail.com>
2024-08-26 12:25:24 -0600

    finished js.3.5

--- JS.3.5/index.js
+++ JS.3.5/index.js
@@ -1,1 +1,49 @@
 //Go to the instructions.md file first thing!
+
+
+function rockPaperScissors (rock, paper, scissors) {
+    Math.floor(Math.random() * 2);
+    if(0){
+        console.log(Rock);
+    }
+    if else(1){
+        console.log(paper);
+    }
+    if else (2){
+        console.log(scissors);
+    }
+    if (player === computer) {
+        result.textContent = 'Its a tie! I chose (computers choice goes here)';
+    }
+    else if (player == 'rock') {
+        if (computer == 'paper') {
+            result.textContent = 'You lose! I chose (computers choice goes here)';
+          
+
+        } else {
+            result.textContent = 'You win! I chose (computers choice goes here)'
+            
+        }
+    }
+    else if (player == 'scissors') {
+        if (computer == 'rock') {
+            result.textContent = 'You lose! I chose (computers choice goes here)';
+            
+        } else {
+            result.textContent = 'You win! I chose (computers choice goes here)';
+            
+        }
+    }
+    else if (player == 'paper') {
+        if (computer == 'scissors') {
+            result.textContent = 'You lose! I chose (computers choice goes here)';
+            
+        } else {
+            result.textContent = 'You win! I chose (computers choice goes here)';
+            p
+        }
+    }
+
+
+
+}
